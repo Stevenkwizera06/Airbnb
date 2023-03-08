b1f2a0cc0e2cfee32060d91fb790a19d8dfb0a05
Stevenkwizera06 <kwiste06@gmail.com>
2023-03-08 12:55:49 +0200

    worked on feedback given

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,30 @@
 import star from "../images/star.png";
 
-export default function Card(props) {
+export default function Card({ item }) {
   let badgeText =
-    props.item.openSpots === 0
+    item.openSpots === 0
       ? "SOLD OUT"
-      : props.item.location === "Online"
+      : item.location === "Online"
       ? "ONLINE"
       : null;
   return (
     <>
       <div className="text-xl flex flex-col gap-3 relative">
-        {/* {openSpots === 0 && <div className="absolute top-2 left-2 bg-white p-2 text-sm rounded-md">SOLD OUT</div>} */}
         {badgeText && (
           <div className="absolute top-2 left-2 bg-white p-2 text-sm rounded-md">
             {badgeText}
           </div>
         )}
-        <img src={props.item.coverImg} alt="pic" className="w-60 rounded-md" />
+        <img src={item.coverImg} alt="pic" className="w-60 rounded-md" />
         <div className="flex gap-2 text-xl">
           <img src={star} alt="star" />
-          <span>{props.item.stats.rating}</span>
-          <span className="text-[#918E9B]">
-            ({props.item.stats.reviewCount})
-          </span>
-          <span className="text-[#918E9B]">.{props.item.location}</span>
+          <span>{item.stats.rating}</span>
+          <span className="text-[#918E9B]">({item.stats.reviewCount})</span>
+          <span className="text-[#918E9B]">.{item.location}</span>
         </div>
-        <p>{props.item.title}</p>
+        <p>{item.title}</p>
         <p>
-          <span className="font-bold">From ${props.item.price}</span> / person
+          <span className="font-bold">From ${item.price}</span> / person
         </p>
       </div>
     </>
